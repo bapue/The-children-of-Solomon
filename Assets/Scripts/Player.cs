556055cb1e55944f16794128a1557a34f8a70b08
bapue <mira00100@gmail.com>
2024-11-20 23:55:40 +0900

    Init

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int Health { get; set; } = 100;
+    public int MaxHealth = 100;
+    
+    public int Mental { get; set; } = 100;
+    public int MaxMental = 100;
+    
+    public int Money { get; set; } = 0;
+    
+    
+    
+}
+
